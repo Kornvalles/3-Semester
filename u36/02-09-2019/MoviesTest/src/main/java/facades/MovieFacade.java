adfa622c953030f4e7b1c11b78e0c813f2867951
Mikkel Kornval Christoffersen <mikkelkornval@gmail.com>
2019-09-04 12:36:25 +0200

    .

---
+++ u36/02-09-2019/MoviesTest/src/main/java/facades/MovieFacade.java
@@ -0,0 +1,72 @@
+package facades;
+
+import entities.Movie;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * Rename Class to a relevant name Add add relevant facade methods
+ */
+public class MovieFacade {
+
+    private static MovieFacade instance;
+    private static EntityManagerFactory emf;
+    
+    //Private Constructor to ensure Singleton
+    private MovieFacade() {}
+    
+    /**
+     * 
+     * @param _emf
+     * @return an instance of this facade class.
+     */
+    public static MovieFacade getFacadeExample(EntityManagerFactory _emf) {
+        if (instance == null) {
+            emf = _emf;
+            instance = new MovieFacade();
+        }
+        return instance;
+    }
+
+    private EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+    
+    //TODO Remove/Change this before use
+//    public long getRenameMeCount(){
+//        EntityManager em = emf.createEntityManager();
+//        try{
+//            long renameMeCount = (long)em.createQuery("SELECT COUNT(r) FROM RenameMe r").getSingleResult();
+//            return renameMeCount;
+//        }finally{  
+//            em.close();
+//        }
+//        
+//    }
+    
+    public String getMovieName(long id) {
+        EntityManager em = getEntityManager();
+        try {
+            String movieName = em.createNamedQuery("SELECT movie");
+        }
+    }
+    
+    public Movie makeMovie(Movie movie) {
+        EntityManager em = getEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.persist(movie);
+            em.getTransaction().commit();
+            return movie;
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+        return null;
+    }
+
+}
