844cd8badc62746b5fd8ce2eaabffd5a3173b765
Mikkel Kornval Christoffersen <mikkelkornval@gmail.com>
2019-09-19 21:22:56 +0200

    Update Person.java

--- u38/18-09-2019/REST_Persons_DTO/src/main/java/entity/Person.java
+++ u38/18-09-2019/REST_Persons_DTO/src/main/java/entity/Person.java
@@ -1,11 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package entity;
 
 import java.io.Serializable;
+import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -22,6 +18,11 @@ public class Person implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+    private String fName;
+    private String lName;
+    private String phone;
+    private Date created;
+    private Date lastEdited;
 
     public Integer getId() {
         return id;
@@ -31,29 +32,4 @@ public class Person implements Serializable {
         this.id = id;
     }
 
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Person)) {
-            return false;
-        }
-        Person other = (Person) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "entity.Person[ id=" + id + " ]";
-    }
-    
 }
