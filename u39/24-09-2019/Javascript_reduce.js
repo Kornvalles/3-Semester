c898f39ff197480874130e01faf0c0bddf26c1bb
Mikkel Kornval Christoffersen <mikkelkornval@gmail.com>
2019-09-25 10:54:37 +0200

    Update Javascript_reduce.js

--- u39/24-09-2019/Javascript_reduce.js
+++ u39/24-09-2019/Javascript_reduce.js
@@ -1,7 +1,35 @@
 //a)
 var all = ["Lars", "Peter", "Jan", "Bo"];
 var str = all.join(", #")
-console.log(str)
+
+//console.log(str)
 
 //b)
 var numbers = [2, 3, 67, 33];
+
+function sumNumbers(number, sum) {
+    return sum + number
+}
+
+var sum = numbers.reduce(sumNumbers)
+
+//console.log(sum)
+
+//c)
+var members = [
+    {name : "Peter", age: 18},
+    {name : "Jan", age: 35},
+    {name : "Janne", age: 25},
+    {name : "Martin", age: 22}]
+
+var averageAge = members.reduce(function(total, member, index, array) {
+    if(index === array.length -1) {
+        return (total + member.age) / array.length
+    } else {
+        return total + member.age
+    }
+}, 0)
+
+console.log(averageAge)
+
+//7
