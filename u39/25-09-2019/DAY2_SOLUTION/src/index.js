064348fd551e530b69436e157a404607cf13c7fb
Mikkel Kornval Christoffersen <mikkelkornval@gmail.com>
2019-09-25 22:16:51 +0200

    Finish first task, what a complete hell

--- u39/25-09-2019/DAY2_SOLUTION/src/index.js
+++ u39/25-09-2019/DAY2_SOLUTION/src/index.js
@@ -1,8 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import jokes from "./jokes";
 
-const allJokes = jokes.getJokes().map(joke => "<li>"+joke+"</li>");
+const allJokes = jokes.getJokes().map(joke => "<li>" + joke + "</li>");
 document.getElementById("jokes").innerHTML = allJokes.join("");
 
-
+document.getElementById("myButton").onclick =
+    function searchForJoke() {
+        var input = document.getElementById("myInput").value
+        const foundJoke = jokes.getJokeById(input)
+        document.getElementById("output").innerHTML = foundJoke
+        document.getElementById("myInput").value = ""
+    }
 
