3f83ee81c18965b142fe8fc5e6d3ce6ab53d01df
Mikkel Kornval Christoffersen <mikkelkornval@gmail.com>
2019-09-25 22:42:06 +0200

    Last practice task

--- u39/25-09-2019/DAY2_SOLUTION/src/index.js
+++ u39/25-09-2019/DAY2_SOLUTION/src/index.js
@@ -10,5 +10,13 @@ document.getElementById("myButton").onclick =
         const foundJoke = jokes.getJokeById(input)
         document.getElementById("output").innerHTML = foundJoke
         document.getElementById("myInput").value = ""
-    }
+    };
 
+document.getElementById("addButton").onclick =
+    function addJoke() {
+        var jokeInput = document.getElementById("addInput").value
+        jokes.addJoke(jokeInput)
+        document.getElementById("addOutput").innerHTML = jokeInput + " was added"
+        const allJokes = jokes.getJokes().map(joke => "<li>" + joke + "</li>");
+        document.getElementById("jokes").innerHTML = allJokes.join("");
+    };
