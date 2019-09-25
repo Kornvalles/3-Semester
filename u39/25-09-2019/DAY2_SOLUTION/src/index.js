f118b98ff7e837bee764a43736253620caa42861
Mikkel Kornval Christoffersen <mikkelkornval@gmail.com>
2019-09-25 23:39:19 +0200

    Deleted all and use fetch

--- u39/25-09-2019/DAY2_SOLUTION/src/index.js
+++ u39/25-09-2019/DAY2_SOLUTION/src/index.js
@@ -1,22 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.css'
-import jokes from "./jokes";
+//import jokes from "./jokes";
 
-const allJokes = jokes.getJokes().map(joke => "<li>" + joke + "</li>");
-document.getElementById("jokes").innerHTML = allJokes.join("");
-
-document.getElementById("myButton").onclick =
-    function searchForJoke() {
-        var input = document.getElementById("myInput").value
-        const foundJoke = jokes.getJokeById(input)
-        document.getElementById("output").innerHTML = foundJoke
-        document.getElementById("myInput").value = ""
-    };
-
-document.getElementById("addButton").onclick =
-    function addJoke() {
-        var jokeInput = document.getElementById("addInput").value
-        jokes.addJoke(jokeInput)
-        document.getElementById("addOutput").innerHTML = jokeInput + " was added"
-        const allJokes = jokes.getJokes().map(joke => "<li>" + joke + "</li>");
-        document.getElementById("jokes").innerHTML = allJokes.join("");
-    };
+document.getElementById("myButton").addEventListener("click", function() {
+    fetch('https://studypoints.info/jokes/api/jokes/period/hour')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("myDiv").innerHTML = data.joke
+        })
+})
