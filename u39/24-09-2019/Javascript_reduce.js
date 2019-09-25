a81895bf3d4baee7388fc3739efb974cc33ba940
Mikkel Kornval Christoffersen <mikkelkornval@gmail.com>
2019-09-25 11:26:05 +0200

    this exercises

--- u39/24-09-2019/Javascript_reduce.js
+++ u39/24-09-2019/Javascript_reduce.js
@@ -30,6 +30,31 @@ var averageAge = members.reduce(function(total, member, index, array) {
     }
 }, 0)
 
-console.log(averageAge)
+//console.log(averageAge)
 
+//this
 //7
+//apply & call
+function f1() {
+   return this.name + ": " + this.phone;
+}
+var a = {number : 2}
+var b = {name : 'Mikkel', phone : 31675555}
+
+//console.log(f1.call(a))
+console.log(f1.apply(b))
+
+/*------------------------------------------------------------------------------------------*/
+//bind
+function f2() {
+    return this.forEach(element => {
+        console.log(element.name + ": " + element.phone)
+    });
+}
+
+var all = [{ name: "Lars", phone: 1234567 }, { name: "Peter", phone: 675843 },
+{ name: "Jan", phone: 98547 }, { name: "Bo", phone: 79345 }];
+
+var g1 = f2.bind(all)
+
+g1();
