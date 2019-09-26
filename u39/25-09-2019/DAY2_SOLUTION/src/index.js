fa5ff315a3d73dc4eb1151b5c9e256d7d272fadb
Mikkel Kornval Christoffersen <mikkelkornval@gmail.com>
2019-09-26 11:19:33 +0200

    Solved but I get an error
    TypeError: null is not an object (evaluating 'document.getElementById(side).addEventListener')

--- u39/25-09-2019/DAY2_SOLUTION/src/index.js
+++ u39/25-09-2019/DAY2_SOLUTION/src/index.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.css'
 //import jokes from "./jokes";
 
-document.getElementById("myButton").addEventListener("click", function() {
+document.getElementById("myButton").addEventListener("click", function () {
     fetch('https://studypoints.info/jokes/api/jokes/period/hour')
         .then(response => response.json())
         .then(data => {
@@ -16,3 +16,14 @@ setInterval(() => {
             document.getElementById("myDiv").innerHTML = data.joke
         })
 }, 3600000);
+
+function cloverClick(side) {
+    document.getElementById(side).addEventListener("click", function () {
+        alert(side.charAt('0').toUpperCase() + side.substr(1))
+    })
+}
+
+cloverClick("north");
+cloverClick("south");
+cloverClick("east");
+cloverClick("west");
