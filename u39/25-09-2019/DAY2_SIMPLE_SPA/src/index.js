641cd9d07543a845f2c161b8dda0da6b8e276831
Mikkel Kornval Christoffersen <mikkelkornval@gmail.com>
2019-09-27 13:08:34 +0200

    Finish DAY2, finally

--- u39/25-09-2019/DAY2_SIMPLE_SPA/src/index.js
+++ u39/25-09-2019/DAY2_SIMPLE_SPA/src/index.js
@@ -26,14 +26,32 @@ function jsonToString(Obj) {
            "Age: " + Obj.age;
 }
 
+function handleHttpErrors(res){
+    if(!res.ok){
+      return Promise.reject({status: res.status, fullError: res.json() })
+    }
+    return res.json();
+   }
+   
 
+//Get user by id
 document.getElementById("myButton").onclick = function() {
     var id = document.getElementById("myInput").value
     fetch('http://localhost:3333/api/users/' + id)
-        .then(response => response.json())
+        .then(handleHttpErrors)
         .then(data => {
-            document.getElementById("myPTag").innerHTML = jsonToString(data)
+            document.getElementById("ageInput").value = data.age
+            document.getElementById("nameInput").value = data.name
+            document.getElementById("genderInput").value = data.gender
+            document.getElementById("emailInput").value = data.email
         })
+        .catch(err =>{
+            if(err.status){
+              err.fullError.then(e=> console.log(e.detail))
+            }
+            else{console.log("Network error"); }
+         });
+        
 }
 
 function request(URL, method, body) {
@@ -52,6 +70,7 @@ function request(URL, method, body) {
     return fetch(URL, makeOptions(method, body));
 }
 
+//Add user
 document.getElementById("addUserButton").onclick = function() {
     const user = {
         "age" : document.getElementById("ageInput").value,
@@ -60,9 +79,29 @@ document.getElementById("addUserButton").onclick = function() {
         "email" : document.getElementById("emailInput").value
     }
     request('http://localhost:3333/api/users/', "POST", user)
-    fetch('http://localhost:3333/api/users')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById("myTable").innerHTML = makeMyTable(data).join(" ")
-        });
+        .then(handleHttpErrors)
+        .catch(err =>{
+            if(err.status){
+              err.fullError.then(e=> console.log(e.detail))
+            }
+            else{console.log("Network error"); }
+         });
+}
+
+//Edit user
+document.getElementById("editUserButton").onclick = function() {
+    let userId = document.getElementById("myInput").value
+    const user = {
+        "age" : document.getElementById("ageInput").value,
+        "name" : document.getElementById("nameInput").value,
+        "gender" : document.getElementById("genderInput").value,
+        "email" : document.getElementById("emailInput").value
+    }
+    request('http://localhost:3333/api/users/' + userId, "PUT", user)
+}
+
+//Delete user
+document.getElementById("deleteUserButton").onclick = function() {
+    let userId = document.getElementById("myInput").value
+    request('http://localhost:3333/api/users/' + userId, "DELETE")
 }
