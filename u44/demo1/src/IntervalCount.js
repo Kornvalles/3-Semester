91f990517978a919f304da5c6d863ec56f44a498
Mikkel Kornval Christoffersen <mikkelkornval@gmail.com>
2019-10-29 22:14:22 +0100

    Added decrementing button

--- u44/demo1/src/IntervalCount.js
+++ u44/demo1/src/IntervalCount.js
@@ -1,22 +1,52 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 //useState - Change what is shown
 //useEffect - Always run after render()
 export default function IntervalCount() {
     const [count, setCount] = useState(0);
+    const [negativeCount, setNegativeCount] = useState(false);
+    const [startCount, setStartCount] = useState(false);
+    //Incrementing
+    useEffect(function () {
 
-    useEffect(function() {
+        if (startCount === false) {
+            return
+        }
+
         const s = setInterval(() => {
-            setCount(count=>count+1)
-        },500)
+            setCount(count => count + 1)
+        }, 500)
 
         //Clears the setInterval to prevent state issues
         return () => clearInterval(s);
 
-    },[])
+    }, [startCount])
+    //Decrementing
+    useEffect(function () {
+
+        if (negativeCount === false) {
+            return
+        }
+
+        const s = setInterval(() => {
+            setCount(count => count - 1)
+        }, 500)
+
+        //Clears the setInterval to prevent state issues
+        return () => clearInterval(s);
+
+    }, [negativeCount])
     return (
         <div>
             <h1>{count}</h1>
+            <button onClick={() => {
+                setStartCount(!startCount)
+                setNegativeCount(false)
+            }}>Start Count Up</button>
+            <button onClick={() => {
+                setStartCount(false)
+                setNegativeCount(!negativeCount)
+            }}>Start Count Down</button>
         </div>
     )
 }
